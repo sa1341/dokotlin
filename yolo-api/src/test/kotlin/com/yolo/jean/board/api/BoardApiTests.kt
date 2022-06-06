f726cce2f40be06de13571bec04fe00031a3a37d
임준영 <a79007714@gmail.com>
2022-06-07 00:15:46 +0900

    feat:  RestDoc PathVariable을 사용한 게시글 조회 API doc 문서 작성

--- yolo-api/src/test/kotlin/com/yolo/jean/board/api/BoardApiTests.kt
+++ yolo-api/src/test/kotlin/com/yolo/jean/board/api/BoardApiTests.kt
@@ -11,10 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
+import org.springframework.restdocs.payload.FieldDescriptor
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.*
+import org.springframework.restdocs.request.RequestDocumentation.parameterWithName
+import org.springframework.restdocs.request.RequestDocumentation.pathParameters
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
@@ -23,7 +28,45 @@ class BoardApiTests: RestDocumentTests() {
     @Autowired
     lateinit var objectMapper: ObjectMapper
 
-    @DisplayName("BoardApi TEST01 - 게시글 저장 테스트")
+    @DisplayName("BoardApi TEST01 - 게시글 조회 테스트")
+    @Test
+    fun 게시글을_조회한다() {
+
+        // given
+        val boardId: Long = 1;
+        val responseBody = BoardDto("임준영", "아프니까 환자지", "스트레스 받지말고 살자...")
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.get("/api/v1/boards/{id}", boardId)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isOk
+        ).andExpect(
+            jsonPath("$.author").value(responseBody.author)
+        ).andExpect(
+            jsonPath("$.title").value(responseBody.title)
+        ).andExpect(
+            jsonPath("$.content").value(responseBody.content)
+        ).andDo(
+            document("board/getBoard",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                pathParameters(
+                    parameterWithName("id").description("게시글 Id")
+                ),
+                responseFields(
+                    fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시글 Id"),
+                    fieldWithPath("author").type(JsonFieldType.STRING).description("게시글 작성자"),
+                    fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 제목"),
+                    fieldWithPath("content").type(JsonFieldType.STRING).description("게시글 내용"),
+                    fieldWithPath("createdAt").type(JsonFieldType.STRING).description("게시글 생성일자"),
+                    fieldWithPath("updatedAt").type(JsonFieldType.STRING).description("게시글 갱신일자"),
+                    fieldWithPath("replies").type(JsonFieldType.ARRAY).description("게시글 댓글목록")
+                )
+            )
+        )
+    }
+
+    @DisplayName("BoardApi TEST02 - 게시글 저장 테스트")
     @Test
     fun 게시글을_저장한다() {
 
